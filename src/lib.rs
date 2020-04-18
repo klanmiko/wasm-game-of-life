d4b5f521efaeda410b43d25740c2964770de95aa
Kaelan Mikowicz <eternity01033@gmail.com>
2020-04-17 20:29:33 -0700

    remove automata cell

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ use wasm_bindgen::prelude::*;
 pub struct Board {
     rows: u16,
     cols: u16,
-    cells: Vec<AutomataCell>
+    cells: Vec<CellState>
 }
 
 #[wasm_bindgen]
@@ -23,31 +23,29 @@ impl Board {
         Board{
             rows: rows,
             cols: cols,
-            cells: vec![AutomataCell{ state: CellState::Dead }; (rows * cols) as usize]
+            cells: vec![CellState::Dead; (rows * cols) as usize]
         }
     }
 
     pub fn toggle_cell(&mut self, x: u16, y: u16) -> Option<CellState> {
         let index = self.index(x, y);
-        self.cells.get_mut(index).map(|cell| {
-            cell.state = match cell.state {
+        self.cells.get_mut(index).map(|cell|
+            match cell {
                 CellState::Alive => CellState::Dead,
                 CellState::Dead => CellState::Alive
-            };
-            return cell.state;
-        })
+            })
     }
 
     pub fn cells(&self) -> Vec<u8> {
-        self.cells.iter().map(|cell| (*cell).into()).collect()
+        self.cells.iter().map(|n| (*n).into()).collect()
     }
 
     fn index(&self, x: u16, y: u16) -> usize {
         (x * self.cols + y) as usize
     }
 
-    fn get_neighbors(&self, x: u16, y: u16) -> [AutomataCell; 8] {
-        let mut neighbors: [AutomataCell; 8] = [AutomataCell{ state: CellState::Dead }; 8];
+    fn get_neighbors(&self, x: u16, y: u16) -> [CellState; 8] {
+        let mut neighbors: [CellState; 8] = [CellState::Dead; 8];
         let mut count = 0;
         for nj in max(0, x as i32 - 1) as u16..min(x + 2, self.cols) {
             for ni in max(0, y as i32 - 1) as u16..min(self.rows, y + 2) {
@@ -61,7 +59,7 @@ impl Board {
     }
 
     pub fn update(&mut self) {
-        let mut new_cells: Vec<AutomataCell> = vec![AutomataCell{ state: CellState::Dead}; self.cells.len()];
+        let mut new_cells = vec![CellState::Dead; self.cells.len()];
         for i in 0..self.rows {
             for j in 0..self.cols {
                 let index = self.index(i, j);
@@ -79,31 +77,23 @@ pub enum CellState {
     Alive
 }
 
-#[derive(Copy, Clone)]
-struct AutomataCell {
-    pub state: CellState
-}
-
-
-impl From<AutomataCell> for u8 {
-    fn from(cell: AutomataCell) -> u8 {
-        match cell.state {
+impl From<CellState> for u8 {
+    fn from(cell: CellState) -> u8 {
+        match cell {
             CellState::Alive => 1,
             CellState::Dead => 0
         }
     }
 }
 
-impl AutomataCell {
-    fn update_cell(&self, neighbors: &[AutomataCell]) -> AutomataCell {
-        let count = neighbors.iter().map(|n| n.state as u8).sum();
+impl CellState {
+    fn update_cell(&self, neighbors: &[CellState]) -> CellState {
+        let count = neighbors.iter().map(|n| *n as u8).sum();
     
-        AutomataCell{
-            state: match (self.state, count) {
-                (CellState::Alive, 2..=3) => CellState::Alive,
-                (CellState::Dead, 3) => CellState::Alive,
-                _ => CellState::Dead
-            }
+        match (self, count) {
+            (CellState::Alive, 2..=3) => CellState::Alive,
+            (CellState::Dead, 3) => CellState::Alive,
+            _ => CellState::Dead
         }
     }
 }
